ba20a6105d370ab76a3006774504024c4c65ee4c
Sudhan007 <sudhancool87@gmail.com>
2024-03-25 10:21:41 +0530

    deployment-failed

--- client/src/routes/newform/+server.js
+++ client/src/routes/newform/+server.js
@@ -63,29 +63,28 @@ export async function POST({ request }) {
     }
 }
 
-export async function POST({ request }) {
-    const data = await request.formData()
+// export async function POST({ request }) {
+//     const data = await request.formData()
 
-    const email1 = data.get('email1')
-    try {
+//     const email1 = data.get('email1')
+//     try {
 
-        const newemail = await prisma.footerForm.create({
-            data: {
-                email1: email1.toString()
-            }
-        })
+//         const newemail = await prisma.footerForm.create({
+//             data: {
+//                 email1: email1.toString()
+//             }
+//         })
 
-        return new Response(JSON.stringify({
-            ok: true,
-            data: newemail
-        }))
-    } catch (error) {
-        return new Response(JSON.stringify({
-            ok: false,
-            message: error.message
-        }))
-    }
+//         return new Response(JSON.stringify({
+//             ok: true,
+//             data: newemail
+//         }))
+//     } catch (error) {
+//         return new Response(JSON.stringify({
+//             ok: false,
+//             message: error.message
+//         }))
+//     }
 
-}
+// }
 
-// kwndw
